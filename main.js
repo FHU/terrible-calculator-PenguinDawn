7d8fa6ea5f9e3c330492a89dd278ad07ec9bc7ce
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-08-27 16:42:38 -0500

    Calc

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-// Step 1 - Find all the elements we need to interact with
-const num1Input = document.getElementById('number1')
-const num2Input = document.getElementById('number2')
-const operatorSelect = document.getElementById('operator')
-const calculateButton = document.getElementById('calculateButton')
-const resultSpan = document.getElementById('result')
+// Step 1 - find elements we need to interact with
+const num1Holder= document.getElementById("number1");
+const num2Holder= document.getElementById("number2");
+const operatorSelect = document.getElementById("operator");
+const calculateButton = document.getElementById("calculate");
+const result = document.getElementById("result");
 
-// Step 2 - Add an event listener to the button to call a function when clicked
-calculateButton.addEventListener('click', calculate)
+
 
 function calculate() {
-    const num1 = parseFloat(num1Input.value)
-    const num2 = parseFloat(num2Input.value)
-    const operator = operatorSelect.value;
+    let num1 = parseFloat(num1Holder.value);
+    let num2 = parseFloat(num2Holder.value);
+    let used = operatorSelect.value;
+    let answer = 0;
 
-    let result
-    if( operator === "+") {
-        result = num1 + num2
+    if(used === "+") {
+        answer = num1 + num2;
+    } else if(used === "-") {
+        answer = num1 - num2;
+    } else if(used === "*") {
+        answer = num1 * num2;
+    } else if (used === "/"){
+        answer = num1 / num2;
     }
-    
-    // Step 3 - update the result span with the result of the calculation
-    resultSpan.innerText = result;
+
+    result.innerText = answer;
 }
+
+calculateButton.addEventListener("click", calculate);
